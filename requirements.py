5ab2df758f97be832525b50a2402adeb5e0f71f6
Yashraj306 <yashrajjadhav306@gmail.com>
2025-07-11 14:14:26 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,7 +1,7 @@
 streamlit==1.35.0
 easyocr==1.6.2
 torch==1.13.1
-opencv-python-headless==4.8.0.74
-Pillow==10.2.0
+torchvision==0.14.1
 numpy==1.24.4
-
+opencv-python-headless==4.7.0.72
+Pillow==10.2.0
